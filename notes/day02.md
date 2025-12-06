72a7155de281c987dc2fdf4e6e96853208a91001
Eric Ritchey <eritchey@indeed.com>
2025-12-06 13:24:31 -0800

    Day 2, Part 1&2; solved

--- notes/day02.md
+++ notes/day02.md
@@ -36,17 +36,50 @@ The rest of the ranges contain no invalid IDs.
 
 * The example produces 1227775554.
 
-## Pseudo-code
+## Pseudo-code - part 1
 
-```
+```plaintext
+
+let invalidIds = [];
+
 for each range in the sequence
   split each range into first and last ID on the -
     firstId = first[0]
     lastId = last[1]
     convert firstId and lastId to integers
 
-    ignore firstId or lastId if:
-      either has a leading zero
-      does not have an even number of digits
+    // Loop through each ID in the range
+    for (let i = firstId; i <= lastId; i++)
+      const currentId = i;
 
+      // Convert currentId to string
+      const currentIdString = currentId.toString();
+      const currentIdLength = currentIdString.length;
+
+      // skip the currentId if it is an odd number of digits (length is odd)
+      const idIsOdd = currentIdLength % 2 !== 0;
+
+      // move to the next if the currentId is odd
+      if (idIsOdd) continue;
+
+      // Split currentId into 2 halves and check them against each other for equality.
+      const currentIdHalf = currentIdLength / 2;
+      const leftSide = currentIdString.substring(0, currentIdHalf);
+      const rightSide = currentIdString.substring(currentIdHalf);
+
+      // if the left side is equal to the right side, add it to the invalidIds array
+      if (leftSide === rightSide)
+        invalidIds.push(currentId);
+
+  // Add all invalidIds together
+  const sum = invalidIds.reduce((a, b) => a + b, 0);
+  console.log(sum); // expected output: 1227775554
 ```
+
+## Pseudo-code - part 2
+
+* Skip single-digit numbers (length < 2)
+* Find all divisors of the **length** that are <= `length / 2` since that's the longest valid pattern.
+* For each divisor `d`, check if repeating the **first `d` characters** works
+* if any divisor works => it's invalid => add to array
+* sum up the invalidIds.
--- src/solutions/day02.ts
+++ src/solutions/day02.ts
@@ -2,13 +2,131 @@ import { parseLines } from '../utils/parse';
 
 export function solveDay02Part1(input: string): number {
   const lines = parseLines(input);
-  // TODO: Implement solution
-  return 0;
+
+  const invalidIds: number[] = [];
+
+  // split the line into ranges
+  const ranges = lines[0].split(',');
+
+  // split the range into first and last ID
+  ranges.forEach((range) => {
+
+    const [firstId, lastId] = range.split('-');
+    const firstIdInt = parseInt(firstId);
+    const lastIdInt = parseInt(lastId);
+
+    // console.log(firstIdInt, lastIdInt);
+
+    // Loop through each ID in the range
+    for (let i = firstIdInt; i <= lastIdInt; i++) {
+
+      const currentId = i;
+      // console.log(currentId);
+
+      // Convert currentId to string
+      const currentIdString = currentId.toString();
+      const currentIdLength = currentIdString.length;
+
+      // skip the currentId if the length is odd.
+      if (currentIdLength % 2 !== 0) {
+        continue;
+      }
+
+      // console.log(currentIdString, currentIdLength);
+
+      const currentIdHalf = currentIdLength / 2;
+      const leftSide = currentIdString.substring(0, currentIdHalf);
+      const rightSide = currentIdString.substring(currentIdHalf);
+
+      // console.log(leftSide, rightSide);
+
+      // if the left is exactly equal to the right, add the currentId to the invalidIds array
+      if (leftSide === rightSide) {
+        invalidIds.push(currentId);
+        // console.log('true',currentId);
+      }
+
+      // console.log(invalidIds);
+    }
+
+
+  });
+  const sum = invalidIds.reduce((a, b) => a + b, 0);
+
+  return sum;
 }
 
 export function solveDay02Part2(input: string): number {
   const lines = parseLines(input);
-  // TODO: Implement solution
-  return 0;
+
+  // create an array to hold all invalid IDs
+  const invalidIds: number[] = [];
+
+  // split the line into ranges
+  const ranges = lines[0].split(',');
+
+  // split the range into first and last ID
+  ranges.forEach((range) => {
+
+    const [firstId, lastId] = range.split('-');
+    const firstIdInt = parseInt(firstId);
+    const lastIdInt = parseInt(lastId);
+
+    // console.log('firstIdInt', firstIdInt);
+    // console.log('lastIdInt', lastIdInt);
+
+    for (let i = firstIdInt; i <= lastIdInt; i++) {
+      const validDivisors: number[] = [];
+      const currentId = i;
+      // console.log('currentId', currentId);
+
+
+      const currentIdString = currentId.toString();
+      const currentIdLength = currentIdString.length;
+      // console.log('currentIdLength', currentIdLength);
+      // console.log('currentIdString', currentIdString);
+
+      // skip the currentId if the length is less than 2
+      if (currentIdLength < 2) {
+        continue;
+      }
+
+      // find all divisors of the currentIdLength that are <= currentIdLength / 2
+      for (let divisor = 1; divisor <= currentIdLength / 2; divisor++) {
+        if (currentIdLength % divisor === 0) {
+          validDivisors.push(divisor);
+        }
+      }
+      // console.log('validDivisors', validDivisors);
+
+      validDivisors.some((divisor) => {
+        // console.log('divisor', divisor);
+
+        const repeats = currentIdLength / divisor;
+        const substring = currentIdString.substring(0, divisor);
+        // console.log('substring', substring);
+        // console.log('repeats', repeats);
+
+        // console.log(
+        //   `original substring: ${substring}`,
+        //   `repeated substring: ${substring.repeat(repeats)}`,
+        //   `equals: ${substring === substring.repeat(repeats)}`
+        // );
+
+        if (currentIdString === substring.repeat(repeats)) {
+          invalidIds.push(currentId);
+          return true;
+        }
+      });
+    }
+
+
+  });
+
+  const sum = invalidIds.reduce((a, b) => a + b, 0);
+
+  console.log('sum', sum);
+
+  return sum;
 }
 
