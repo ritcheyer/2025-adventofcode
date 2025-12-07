69770aca08a0fa62745840d7f92abae8a3f45a07
Eric Ritchey <eritchey@indeed.com>
2025-12-07 11:20:48 -0800

    Day 05, Part 1 complete

--- notes/day05.md
+++ notes/day05.md
@@ -4,17 +4,32 @@
 
 ### Notes
 
--
+- Instead of looping over groups and mutating the IDs, flip the logic!
+- Loop through the IDs first
+- Exit if an ID is found to be within the range, preventing mutation
+- Increment a counter when a fresh Id is found
+
+
 
 ### Pseudo-code
 
-```js
+```plaintext
+function isIdBetween(id,min,max)
+  logic to determine if id is between min and max
 
+foreach id in ids
+  foreach group in groups
+    split on '-' and map to numbers
+      if isIdBetween(id,min,max)
+        count++
+        break
+      else
+        continue
 ```
 
 ## Part 2
 
 ### Notes
 
--
+
 
--- src/solutions/day05.ts
+++ src/solutions/day05.ts
@@ -1,14 +1,46 @@
-import { parseLines } from '../utils/parse';
+import { parseGroups } from '../utils/parse';
 
+function isIdBetween(num: number, min: number, max: number): boolean {
+  return num >= min && num <= max;
+}
 export function solveDay05Part1(input: string): number {
-  const lines = parseLines(input);
+  const [groups, ids] = parseGroups(input);
 
-  // TODO: Implement solution
-  return 0;
+  /**
+   * This is an optimization of the logic to go with a filtering of the IDs.
+   * Instead of looping over groups and mutating the IDs, flip the logic!
+   * Loop through the IDs first
+   * Exit if an ID is found to be within the range, preventing mutation
+   * Increment a counter when a fresh Id is found
+   */
+  return ids.filter((id) =>
+    groups.some((group) => {
+      const [groupMin, groupMax] = group.split('-').map(Number);
+      return isIdBetween(Number(id), groupMin, groupMax);
+    })).length;
+
+  /**
+   * Keeping this logic around as it was my first successful attemp.
+   *
+
+  let count = 0;
+  ids.forEach((id) => {
+    groups.some((group) => {
+
+      const [groupMin, groupMax] = group.split('-').map(Number);
+
+      if (isIdBetween(Number(id), groupMin, groupMax)) {
+        count++;
+        return true;
+      }
+    });
+  });
+  return count;
+  */
 }
 
 export function solveDay05Part2(input: string): number {
-  const lines = parseLines(input);
+  const lines = parseGroups(input);
 
   // TODO: Implement solution
   return 0;
